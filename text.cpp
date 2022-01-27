48ca9168cb11c32fcf43417b044b0c0a5f8283a8
sup <jeesup0103@gmail.com>
2022-01-27 21:55:58 +0900

    fourth commit

--- text.cpp
+++ text.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <queue>
 using namespace std;
 
 int main(void) {
     int a, b, c;
     a = 1, b = 2, c = 3;
 
+    queue<int> q;
+    q.push(0);
+    //fourth commitment
     return 0;
 }
 
